8bb20d9c313c3216569e31f8589aa38c00ea72a3
Demchenk0 <santadyr2016@gmail.com>
2023-10-06 21:31:40 +0300

    hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React from 'react';
 import styles from './Hero.module.scss';
 
 const Hero = () => {
-  return (
-    <section className={styles.section}>Hero</section>
-  )
-}
+	return (
+		<section className={styles.section}>
+			<div className={styles.container}>
+      <p >Hi my name is</p>
+				<h1 className={styles.name}>
+        Ivan Ivanov
+				</h1>
+        <h2 className={styles.who}> 
+        I'm Strong Junior Developer</h2>
+        <p className={styles.text}>	Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
+					harum aspernatur ab repudiandae corrupti, est eveniet nulla mollitia
+					sint id enim quasi! Vitae recusandae maiores et, ratione atque
+					reiciendis cupiditate!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis labore neque fuga eum. Assumenda dolore laudantium ex, quod earum nam, aperiam totam voluptates vero obcaecati corrupti. Perspiciatis, voluptate laboriosam! Deleniti.</p>
+			</div>
+		</section>
+	);
+};
 
-export default Hero
+export default Hero;
